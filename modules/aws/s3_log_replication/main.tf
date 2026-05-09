18547d1bcd67cf4a51c84fc017e5cff29d88a6ea
Ronald Landheer-Cieslak <rlc@vlinder.ca>
2026-05-09 16:03:32 +0000

    feat(s3_log_replication): add reusable S3 log replication module with BDD tests

---
+++ modules/aws/s3_log_replication/main.tf
@@ -0,0 +1,75 @@
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["s3.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "replication" {
+  name               = var.role_name
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+  tags               = var.tags
+}
+
+data "aws_iam_policy_document" "replication" {
+  statement {
+    actions = [
+      "s3:GetReplicationConfiguration",
+      "s3:ListBucket",
+    ]
+    resources = [var.source_bucket_arn]
+  }
+
+  statement {
+    actions = [
+      "s3:GetObjectVersionForReplication",
+      "s3:GetObjectVersionAcl",
+      "s3:GetObjectVersionTagging",
+    ]
+    resources = ["${var.source_bucket_arn}/*"]
+  }
+
+  statement {
+    actions = [
+      "s3:ReplicateObject",
+      "s3:ReplicateDelete",
+      "s3:ReplicateTags",
+    ]
+    resources = ["${var.destination_bucket_arn}/*"]
+  }
+}
+
+resource "aws_iam_role_policy" "replication" {
+  name   = "${var.role_name}-policy"
+  role   = aws_iam_role.replication.id
+  policy = data.aws_iam_policy_document.replication.json
+}
+
+resource "aws_s3_bucket_versioning" "source" {
+  bucket = var.source_bucket_id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_replication_configuration" "this" {
+  bucket = var.source_bucket_id
+  role   = aws_iam_role.replication.arn
+
+  rule {
+    id     = var.rule_id
+    status = "Enabled"
+
+    destination {
+      bucket        = var.destination_bucket_arn
+      storage_class = var.destination_storage_class
+    }
+  }
+
+  depends_on = [aws_s3_bucket_versioning.source]
+}
