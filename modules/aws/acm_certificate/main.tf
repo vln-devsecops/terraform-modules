798d4c18a705cc7fcd4f2f323dadb4dee1473558
Ronald Landheer-Cieslak <rlc@vlinder.ca>
2026-05-20 22:15:11 +0000

    refactor: Route53 record validation to use deduplicated domain names

--- modules/aws/acm_certificate/main.tf
+++ modules/aws/acm_certificate/main.tf
@@ -10,21 +10,35 @@ resource "aws_acm_certificate" "this" {
   tags = var.tags
 }
 
-resource "aws_route53_record" "validation" {
-  for_each = {
-    for dvo in aws_acm_certificate.this.domain_validation_options :
-    "${dvo.resource_record_name}/${dvo.resource_record_type}" => {
-      name  = dvo.resource_record_name
-      type  = dvo.resource_record_type
-      value = dvo.resource_record_value
-    }...
+locals {
+  # Deduplicate apex/wildcard validation records while keeping stable, plan-time keys.
+  validation_domains_grouped = {
+    for domain in concat([var.domain_name], var.subject_alt_names) :
+    (contains(var.subject_alt_names, "*.${trimprefix(domain, "*.")}") ? "*.${trimprefix(domain, "*.")}" : trimprefix(domain, "*.")) => trimprefix(domain, "*.")...
   }
 
+  validation_domains_by_key = {
+    for key, values in local.validation_domains_grouped : key => values[0]
+  }
+}
+
+resource "aws_route53_record" "validation" {
+  for_each = local.validation_domains_by_key
+
   zone_id = var.route53_zone_id
-  name    = each.value[0].name
-  type    = each.value[0].type
-  ttl     = 60
-  records = [each.value[0].value]
+  name = element([
+    for dvo in aws_acm_certificate.this.domain_validation_options : dvo.resource_record_name
+    if trimprefix(dvo.domain_name, "*.") == each.value
+  ], 0)
+  type = element([
+    for dvo in aws_acm_certificate.this.domain_validation_options : dvo.resource_record_type
+    if trimprefix(dvo.domain_name, "*.") == each.value
+  ], 0)
+  ttl = 60
+  records = [element([
+    for dvo in aws_acm_certificate.this.domain_validation_options : dvo.resource_record_value
+    if trimprefix(dvo.domain_name, "*.") == each.value
+  ], 0)]
 }
 
 resource "aws_acm_certificate_validation" "this" {
